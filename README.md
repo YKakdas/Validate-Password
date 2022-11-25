ce72636159c01b6104259e7db11178c4fee6c4a8
Yasar Kakdas <ykakdas@gmail.com>
2022-11-25 16:07:41 -0500

    Reformated code, updated readme and added example input file.

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Validate-Password
 
-This phyton script checks whether a password is valid or not using regular expressions. A valid password contains at least
-one uppercase letter, at least one lowercase letter and at least one
-digit. A valid password must consist of at least 8 characters.
+This phyton script checks whether a password is valid or not using regular expressions. A valid password must contains at least:
+-   one uppercase letter
+-   one lowercase letter
+-   one digit
+-   total of 8 characters
 
-If the input file’s name is passwords.txt, the program should be run as:
 
-$ python validate_password.py passwords.txt
+## Running instructions
+
+-   File's path should be specified in command line arguments
+
+-   If the input file’s name is `passwords.txt` and located at the same directory that script exists, then the program should be run as:
+
+```
+python validate_password.py passwords.txt
+```
--- validate_password.py
+++ validate_password.py
@@ -1,23 +1,31 @@
-# 161101014, Yasar Can Kakdas, Bil 334
-#!/usr/bin/env python
-import sys
+# !/usr/bin/env python
+import os
 import re
+import sys
 
-password_file=sys.argv.pop();
-output_file=open(password_file[:-4]+'_output.txt','w');
-pattern='^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])\S{8,}$';
-#(?=.*[a-z]) : ensures that at least one lower character
-#(?=.*[A-Z]) : ensures that at least one upper character
-#(?=.*[0-9]) : ensures that at least one digit
-#\S : after ensurances, read characters with this
-#{8,} : means at least 8 characters
-for line in open(password_file) :
-    result=re.findall(pattern,line);
-    if(len(result)==0) :
-        output_file.write('not valid\n');
-    else :
-        output_file.write('valid\n');
+if not len(sys.argv) > 1:
+    print("Could not find a text file that contains passwords to be validated")
+    exit(0)
 
-output_file.close();
+password_file = sys.argv.pop()
 
+if not os.path.exists(password_file):
+    print("Could not find the specified file named: " + password_file)
+    exit(0)
 
+output_file = open(password_file[:-4] + '_output.txt', 'w')
+pattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])\S{8,}$'
+# (?=.*[a-z]) : ensures that at least one lower character
+# (?=.*[A-Z]) : ensures that at least one upper character
+# (?=.*[0-9]) : ensures that at least one digit
+# \S : after satisfying the constraints, read any character but whitespace with this
+# {8,} : means at least 8 characters
+
+for line in open(password_file):
+    result = re.findall(pattern, line)
+    if len(result) == 0:
+        output_file.write('Password: \"' + line.replace("\n", "") + '\" is not valid\n')
+    else:
+        output_file.write('Password: \"' + line.replace("\n", "") + '\" is valid\n')
+
+output_file.close()
